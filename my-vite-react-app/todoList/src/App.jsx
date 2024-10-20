3db1ff191daee2748dfa711fd168cc6d9e91ef75
Gaurav Kushwaha <kushwahagaurav062@gmail.com>
2024-10-20 15:12:24 +0530

    todo

---
+++ my-vite-react-app/todoList/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Todo from './components/todo';
+// import Todo from './components/todo'
+
+function App() {
+
+
+  return (
+    <>
+     <h1 style={{textAlign:"center", margin:"30px 0"}}>Todo Application</h1>
+     <Todo />
+    </>
+  )
+}
+
+export default App;
