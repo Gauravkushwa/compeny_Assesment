6d3e685e083b2875bcac20468434f6e4e8044286
Gaurav Kushwaha <kushwahagaurav062@gmail.com>
2024-10-20 15:50:15 +0530

    these is the todo project

---
+++ my-app/src/components/todo.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import './style.css'
+
+
+function Todo(){
+   
+    const [inputValue, setInputValue] = useState('');
+    const [tasks, setTasks] = useState([]);
+    const [completed, setCompleted] = useState(false);
+
+    const handleSubmit = (e)=> {
+        e.preventDefault();
+        setTask()
+    }
+    const setTask = () => {
+        if (inputValue.trim()) {
+            
+            setTasks([...tasks, { name: inputValue, completed }]);
+            setInputValue('');
+            setCompleted(false);
+        }
+    };
+    const removeTask = (index) => {
+        const newTasks = tasks.filter((_, i) => i !== index);
+        setTasks(newTasks);
+      };
+    
+    return(
+        <>
+         <form onSubmit={handleSubmit}>
+            <label>todo Name</label>
+            <input type="text" placeholder="Enter your todo"
+             value={inputValue}
+             onChange={(e) => setInputValue(e.target.value)}
+             style={{height:"30px", textAlign:"center", outline: "none"}}
+             />
+            <input type="checkbox" 
+            checked={completed}
+            onChange={() => setCompleted(!completed)}
+            />
+            <button onClick={setTask}>Add Task</button>
+
+         </form>
+         <table className="task-table">
+            <thead>
+                <tr>
+                    <th>Task</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                {tasks.map((task, index) => (
+                    <tr key={index}>
+                        <td>{task.name}</td>
+                       { <td>
+                       { task.completed ? "Completed" : "Pendding"}
+                        </td>}
+                        <td>
+                            <button onClick={() => removeTask(index)}>Remove Task</button>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+
+        </>
+    )
+}
+export default Todo;
